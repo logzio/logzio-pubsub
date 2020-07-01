8e45875ddecdb10238f3878cf1746b65fd526697
Roni.sh <ronish31@gmail.com>
2020-07-01 16:21:18 +0300

    Handles listener: default and parameters

--- filebeat-yml-script.py
+++ filebeat-yml-script.py
@@ -3,7 +3,7 @@ from ruamel.yaml import YAML
 
 FILEBEAT_CONF_PATH = "/etc/filebeat/filebeat.yml"
 credentials_files = {}
-
+LISTENERS = ("listener-au.logz.io", "listener-ca.logz.io", "listener-eu.logz.io", "listener-nl.logz.io", "listener-uk.logz.io", "listener-wa.logz.io");
 
 def _add_all_topics():
     yaml = YAML()
@@ -18,8 +18,10 @@ def _add_all_topics():
         for subscriber in publisher["subscriptions"]:
             subscriber_dict = _add_subscriber(publisher, subscriber)
             config_dict["filebeat.inputs"].append(subscriber_dict)
+    listener = pubsub_input["listener"] + ":5015"
+    if not listener.startswith(LISTENERS):
+        listener = "listener.logz.io:5015"
 
-    listener = pubsub_input.get("listener", "listener.logz.io") + ":5015"
     config_dict["output"]["logstash"]["hosts"].append(listener)
     print("this is our listener: " + listener)
     with open(FILEBEAT_CONF_PATH, "w+") as filebeat_yml:
