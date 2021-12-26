5304556312000a63a5cad11cc865493653de1969
AntonKolomiiets <anton.kolomiiets@logz.io>
2021-12-26 11:52:36 +0200

    update docker to support ARM

--- filebeat-yml-script.py
+++ filebeat-yml-script.py
@@ -1,18 +1,18 @@
 import os
-from ruamel.yaml import YAML
+import yaml
 
 FILEBEAT_CONF_PATH = "/etc/filebeat/filebeat.yml"
 credentials_files = {}
 error_msg = "Your listener is invalid."
 
+
 def _add_all_topics():
-    yaml = YAML()
 
     with open("filebeat.yml", "r") as filebeat_yml:
-        config_dict = yaml.load(filebeat_yml)
+        config_dict = yaml.safe_load(filebeat_yml)
 
     with open("pubsub-input.yml", "r") as input_yml:
-        pubsub_input = yaml.load(input_yml)
+        pubsub_input = yaml.safe_load(input_yml)
 
     for publisher in pubsub_input["pubsubs"]:
         for subscriber in publisher["subscriptions"]:
@@ -32,7 +32,8 @@ def _add_all_topics():
 
 def _add_subscriber(publisher, subscriber):
     project_id = publisher["project_id"]
-    credentials_file = publisher.get("credentials_file", str(project_id) + "-credentials.json")
+    credentials_file = publisher.get(
+        "credentials_file", str(project_id) + "-credentials.json")
     subscriber_dict = {
         "type": "google-pubsub",
         "project_id": project_id,
@@ -44,7 +45,7 @@ def _add_subscriber(publisher, subscriber):
                 "logzio_codec": "json",
                 "token": publisher["token"],
                 "type": publisher.get("type", "google-pubsub"),
-            },
+        },
         "fields_under_root": "true",
         "encoding": "utf-8"
     }
